4ead135c47ae504833cbc3ee9cd4f072bb0e35b0
George Darby <george_darby98@hotmail.com>
2025-12-20 14:49:33 -0500

    Fix app server SG HTTPS ingress protocol to tcp

--- security-groups/main.tf
+++ security-groups/main.tf
@@ -76,14 +76,14 @@ resource "aws_security_group" "app_server_security_group" {
     description     = "https access"
     from_port       = 443
     to_port         = 443
-    protocol        = "-1"
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb_security_group.id]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
