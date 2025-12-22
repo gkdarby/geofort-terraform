3163f871518c349bd13cb2f29f33cd8586da4d7a
George Darby <george_darby98@hotmail.com>
2025-12-22 15:53:06 -0500

    Fix RDS subnet vars and subnet group reference

--- rds/main.tf
+++ rds/main.tf
@@ -1,7 +1,7 @@
 # create database subnet group
 resource "aws_db_subnet_group" "database_subnet_group" {
   name        = "${var.project_name}-${var.environment}-database-subnets"
-  subnet_ids  = [var.private_data_subnet_az1.id, var.private_data_subnet_az2.id]
+  subnet_ids  = [var.private_data_subnet_az1_id, var.private_data_subnet_az2_id]
   description = "subnets for database instance"
 
   tags = {
@@ -22,8 +22,9 @@ resource "aws_db_instance" "database_instance" {
   skip_final_snapshot    = true
   availability_zone      = var.availability_zone_1
   identifier             = var.database_instance_identifier
-  snapshot_identifier    = data.aws_db_snapshot.latest_db_snapshot
-  db_subnet_group_name   = aws_db_subnet_group.atabase_subnet_group.name
+  snapshot_identifier    = data.aws_db_snapshot.latest_db_snapshot.id
+  db_subnet_group_name   = aws_db_subnet_group.database_subnet_group.name
   multi_az               = var.multi_az_deployment
   vpc_security_group_ids = [var.database_security_group_id]
 }
+
