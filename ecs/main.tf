d5ae934fd0915fa0d81b4432586a4cb669b52ac3
George Darby <george_darby98@hotmail.com>
2025-12-26 19:51:54 -0500

    Update infrastructure configuration

--- ecs/main.tf
+++ ecs/main.tf
@@ -90,6 +90,6 @@ resource "aws_ecs_service" "ecs_service" {
   load_balancer {
     target_group_arn = var.alb_target_group_arn
     container_name   = "${var.project_name}-${var.environment}-container"
-    container_port   = 80
+    container_port   = 8080
   }
 }
